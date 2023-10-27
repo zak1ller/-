5adc0b8246083d842c4347d8426c7b202f78e2b7
= <=>
2023-10-27 16:45:44 +0900

    6.8 Webtoon Card

--- lib/webtoon/views/home_view.dart
+++ lib/webtoon/views/home_view.dart
@@ -21,16 +21,62 @@ class WebtoonHomeView extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return ListView.separated(
-              scrollDirection: Axis.horizontal,
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                final webtoon = snapshot.data![index];
-                return Text(webtoon.title);
-              },
-              separatorBuilder: (context, index) => const SizedBox(
-                width: 10,
-              ),
+            return Column(
+              children: [
+                Expanded(
+                  child: ListView.separated(
+                    scrollDirection: Axis.horizontal,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 16,
+                      vertical: 16,
+                    ),
+                    itemCount: snapshot.data!.length,
+                    itemBuilder: (context, index) {
+                      final webtoon = snapshot.data![index];
+                      return Column(
+                        children: [
+                          Container(
+                            width: 250,
+                            clipBehavior: Clip.hardEdge,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15),
+                              boxShadow: [
+                                BoxShadow(
+                                  blurRadius: 15,
+                                  offset: const Offset(
+                                    10,
+                                    10,
+                                  ),
+                                  color: Colors.black.withOpacity(0.2),
+                                ),
+                              ],
+                            ),
+                            child: Image.network(
+                              webtoon.thumb,
+                              headers: const {
+                                "User-Agent":
+                                    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
+                              },
+                            ),
+                          ),
+                          const SizedBox(
+                            height: 10,
+                          ),
+                          Text(
+                            webtoon.title,
+                            style: const TextStyle(
+                              fontSize: 18,
+                            ),
+                          ),
+                        ],
+                      );
+                    },
+                    separatorBuilder: (context, index) => const SizedBox(
+                      width: 16,
+                    ),
+                  ),
+                ),
+              ],
             );
           } else {
             return const Center(
